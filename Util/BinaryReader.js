6b4fb51f9c94e69e8b0d72d44768f444fc415f05
Nick Smith <portification@gmail.com>
2015-09-27 10:04:47 +0200

    Try fixing ReadString

--- Util/BinaryReader.js
+++ Util/BinaryReader.js
@@ -47,9 +47,30 @@ BinaryReader.prototype.ReadDouble = function() {
   return value;
 };
 
+BinaryReader.Read7BitEncodedInt = function() {
+    // Read out an Int32 7 bits at a time.  The high bit
+    // of the byte when on means to continue reading more bytes.
+    var count = 0;
+    var shift = 0;
+    var b;
+    do {
+        // Check for a corrupted stream.  Read a max of 5 bytes.
+        // In a future version, add a DataFormatException.
+        if (shift == 5 * 7)  // 5 bytes max per Int32, shift += 7
+            throw new Error("Format_Bad7BitInt32");
+
+        // ReadByte handles end of stream cases for us.
+        b = this.ReadByte();
+        count |= (b & 0x7F) << shift;
+        shift += 7;
+    } while ((b & 0x80) != 0);
+    return count;
+}
+
 BinaryReader.prototype.ReadString = function() {
-  var test = this.ReadByte();
-  if (test >= 127) throw new Error("watch out");
+  //var test = this.ReadByte();
+  //if (test >= 127) throw new Error("watch out");
+  var test = this.Read7BitEncodedInt();
   var data = this.Read(test);
   var decoded = String.fromCharCode.apply(null, new Uint8Array(data));
   return decoded;
